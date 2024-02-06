0197fed16a3043c0fbd0fef87808e4772e92409c
yamatium <rodrigofleitas910@hotmail.com>
2024-02-06 10:08:07 -0300

    test

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -26,7 +26,9 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
+        #pass
+        alert("titulo", "Esto no anda, funciona")
+        
 
 
 if __name__ == "__main__":
